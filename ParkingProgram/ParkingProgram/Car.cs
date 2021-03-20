783cd49c8c42ff1616d9539910d8742deb133856
babakranjbar <81034028+babakranjbar@users.noreply.github.com>
2021-03-20 19:48:52 +0330

    Add files via upload

---
+++ ParkingProgram/ParkingProgram/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingProgram
+{
+    class Car : Vehicle , IParkable
+    {
+        Car(int ID) : base(ID) { }
+
+        public int Entryhour
+        {
+
+            get
+            {
+                return Entryhour;
+            }
+            set
+            {
+                if (enterHour >= 1 & enterHour <= 24)
+                {
+                    Entryhour = enterHour;
+                }
+            }
+        }
+
+        public int ExitHour
+        {
+            get
+            {
+                return ExitHour;
+            }
+            set
+            {
+                if (exitHour >= 1 & exitHour <= 24)
+                {
+                    ExitHour = exitHour;
+                }
+            }
+        }
+
+        public int EntrancePrice(int EntrancePrice)
+        {
+            return EntrancePrice = 50000;
+        }
+
+        public int GetCost(int EntrancePrice, int Entryhour, int ExitHour, int HourPrice)
+        {
+            int Time = ExitHour - Entryhour;
+            int perhours = Time * HourPrice;
+            int GetCost = perhours + EntrancePrice;
+            return GetCost;
+        }
+
+        public int HourPrice(int HourPrice)
+        {
+            return HourPrice = 20000;
+        }
+    }
+}
---
+++ ParkingProgram/ParkingProgram/IParkable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingProgram
+{
+    interface IParkable
+    {
+        int Entryhour { get; set; }
+        int ExitHour { get; set; }
+        int EntrancePrice(int EntrancePrice);
+        int HourPrice(int HourPrice);
+        int GetCost(int EntrancePrice, int Entryhour, int ExitHour, int HourPrice);
+    }
+}
