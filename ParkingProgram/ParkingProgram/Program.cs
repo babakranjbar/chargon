6d673b2f28b619e20fc17222af94bb0913c88706
babakranjbar <81034028+babakranjbar@users.noreply.github.com>
2021-03-20 19:50:03 +0330

    Add files via upload

---
+++ ParkingProgram/ParkingProgram/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingProgram;
+
+namespace ParkingProgram
+{
+    class Program
+
+    {
+        
+
+        static void MainMenu()
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("Parking System Program");
+            Console.WriteLine("******************************");
+            Console.WriteLine("1. Add vehicle");
+            Console.WriteLine("2. Remove vehicle");
+            Console.WriteLine("q. Quit");
+        }
+        static void Main(string[] args)
+        {
+
+            MainMenu();
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("******************************");
+                Console.WriteLine("Add vehicle");
+                Console.WriteLine("******************************");
+                Console.WriteLine("1. car  ");
+                Console.WriteLine("2. bike");
+                string input2 = Console.ReadLine();
+                if (input2 == "1")
+                {
+                    Car cars = new Car();
+                }
+                
+
+
+
+
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("2");
+            }
+            else if (input == "q" | input == "Q")
+            {
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ ParkingProgram/ParkingProgram/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingProgram
+{
+    abstract class Vehicle
+    {
+        public Vehicle(int id) => ID = id;
+
+        public int ID; //feild for id
+        public enum VehicleType
+        {
+            car = 1,
+            bike = 2
+        }
+    }
+}
